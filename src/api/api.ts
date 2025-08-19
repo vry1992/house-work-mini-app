7e2a397159a0225118010976ed7d4ccdabc0af72
Vitalii Rybachuk <vitalii.rybachuk@infopulse.com>
2025-08-19 17:15:12 +0300

    test api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.BASE_URL,
+  // baseURL: import.meta.env.BASE_URL,
+  baseURL: 'http://localhost:3000',
 });
